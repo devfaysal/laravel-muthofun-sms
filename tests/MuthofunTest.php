2eaec8a6d2a92c6915000644e2fea921bc058387
Faysal Ahamed <devfaysal@gmail.com>
2026-06-20 09:13:00 +0600

    Laravel 13 Support

--- tests/MuthofunTest.php
+++ tests/MuthofunTest.php
@@ -2,17 +2,26 @@
 
 namespace Devfaysal\Muthofun\Tests;
 
+use Devfaysal\Muthofun\Muthofun;
+use Devfaysal\Muthofun\MuthofunServiceProvider;
 use Orchestra\Testbench\TestCase;
-
 
 class MuthofunTest extends TestCase
 {
-
-    /** @test */
-
-    public function it_has()
+    protected function getPackageProviders($app): array
     {
-        
+        return [
+            MuthofunServiceProvider::class,
+        ];
     }
 
+    protected function defineEnvironment($app): void
+    {
+        $app['config']->set('muthofun.apiKey', 'test-api-key');
+    }
+
+    public function test_it_registers_the_muthofun_service(): void
+    {
+        $this->assertInstanceOf(Muthofun::class, $this->app->make('muthofun'));
+    }
 }
